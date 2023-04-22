4c9a1ed243bddd783a3181cacc58bef5468b74d9
AtefBenH <ben_hasnaoui_atef@yahoo.fr>
2023-04-22 18:50:20 +0100

    Assignment: Dojo Fruit Store

--- server.py
+++ server.py
@@ -8,6 +8,11 @@ def index():
 @app.route('/checkout', methods=['POST'])         
 def checkout():
     print(request.form)
+    customer_name = request.form ['first_name'] + request.form ['last_name']
+    count = int(request.form['apple'])+int(request.form['strawberry'])+int(request.form['raspberry'])
+    print("-"*100)
+    print(f"Charging {customer_name} for {count} fruits")
+    print("-"*100)
     return render_template("checkout.html")
 
 @app.route('/fruits')         
